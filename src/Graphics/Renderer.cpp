5ff0b41529b45cdac43317903fefe3c8da765b5e
Bahadır İyol <bahadiriyol12@gmail.com>
2025-12-07 23:08:32 +0300

    Update Renderer.cpp

--- src/Graphics/Renderer.cpp
+++ src/Graphics/Renderer.cpp
@@ -44,7 +44,6 @@ void Renderer::DrawSimulation(const World& world) {
             Color c_fire = { 255, 0, 0, 255 };
             Color c_plasma = { 255, 255, 255, 255 };
 
-            //TODO SWITCH CASE??
             if (temp < 0.0f) {
                 c = c_cold;
             }
@@ -130,67 +129,3 @@ void Renderer::DrawUI(int& currentTool) {
             currentTool = elements[i].id;
     }
 }
-
-
-//TODO Advanced Thermal Mode
-/*
-void Renderer::DrawSimulation(const World& world) {
-    const std::vector<int>& grid = world.GetGridData();
-    const std::vector<float>& gridTemp = world.GetTempData();
-
-    for (int i = 0; i < (int)grid.size(); i++) {
-        Color c = BLACK;
-
-        if (thermalMode) {
-            //! --- THERMAL MODE RENDER ---
-            float temp = gridTemp[i];
-
-            // Color Palette
-            Color c_cold = { 0, 0, 150, 255 };     
-            Color c_ambient = { 0, 120, 255, 255 };   
-            Color c_room = { 0, 255, 255, 255 };   
-            Color c_warm = { 0, 255, 0, 255 };     
-            Color c_hot = { 255, 255, 0, 255 };   
-            Color c_fire = { 255, 0, 0, 255 };    
-            Color c_plasma = { 255, 255, 255, 255 };
-
-            
-            if (temp < 0.0f) {
-                c = c_cold;
-            }
-            else if (temp < 25.0f) {
-                float t = (temp - 0.0f) / 25.0f;
-                c = ColorLerp(c_ambient, c_room, t);
-            }
-            else if (temp < 100.0f) {
-                float t = (temp - 25.0f) / 75.0f;
-                c = ColorLerp(c_room, c_warm, t);
-            }
-            else if (temp < 400.0f) {
-                float t = (temp - 100.0f) / 300.0f;
-                c = ColorLerp(c_warm, c_hot, t);
-            }
-            else if (temp < 1000.0f) {
-                float t = (temp - 400.0f) / 600.0f;
-                c = ColorLerp(c_hot, c_fire, t);
-            }
-            else {         
-                float t = (temp - 1000.0f) / 1000.0f;
-                if (t > 1.0f) t = 1.0f;
-                c = ColorLerp(c_fire, c_plasma, t);
-            }
-        }
-        else {
-            //! --- STANDARD RENDER ---
-            if (grid[i] != EMPTY) {
-                c = GetElementColor(grid[i]);
-                //! Add noise for liquid/gas visuals
-                if (grid[i] == ACIDIC_WATER) c = ColorLerp(c, WHITE, GetRandomValue(-10, 10) / 100.0f);
-                if (grid[i] == FIRE) c = (GetRandomValue(0, 2) == 0) ? ORANGE : RED;
-                if (grid[i] == SAND) c = ColorLerp(c, BLACK, GetRandomValue(0, 5) / 100.0f);
-                if (grid[i] == ACID) c = (GetRandomValue(0, 3) == 0) ? LIME : c;
-            }
-        }
-        ImageDrawPixel(&simImage, i % simWidth, i / simWidth, c);
-    }
-*/
